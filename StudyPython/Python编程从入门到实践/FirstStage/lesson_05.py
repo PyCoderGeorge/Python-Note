accbd762c92fc6058bc3ebaf88f343e8d71346f3
刘峻廷 <liujunting@liujuntingdeMBP.lan>
2017-09-25 21:41:06 +0800

    字典

---
+++ StudyPython/Python编程从入门到实践/FirstStage/lesson_05.py
@@ -0,0 +1,20 @@
+# 第五章 if语句
+## 课后习题
+
+# 第六章 字典
+
+## 遍历字典
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'java',
+    'twitch': 'php',
+}
+
+for name, language in favorite_languages.items() :
+    print(name.title() + "'s favorite language is " + language.title() + '.');
+
+
+
+
